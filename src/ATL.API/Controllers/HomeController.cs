d8090cca8334a989ea194e0c8ddf2e1e74669446
Gabriel Ichim <ichimgabriel75@gmail.com>
2019-05-02 13:45:42 +0100

    test build

---
+++ src/ATL.API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATLAPI.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Get() => Ok("ATL API");
+    }
+}
